5e07422db0c65eb5e367627584c935aaabd4cfb2
Sjoerd Wouters <sjoerdwouters2004@gmail.com>
2024-12-02 19:56:51 +0100

    restructured

--- src/Game.cs
+++ src/Game.cs
@@ -1,55 +1,43 @@
 using System.Drawing;
 using System.Numerics;
-
 using Lean;
-
-namespace Game;
 
 public static class Game
 {
     static Sprite sprite;
-
     static AudioClipWav music;
-    static AudioClipWav effect;
 
     static void Main()
     {
+        // setup window
         Windowing.SetupWindow(800, 600, "This is a game window", OnLoad, OnUpdate, OnRender);
     }
 
     public static void OnLoad()
     {
+        // load resources
         sprite = new Sprite("res/png/player.png");
-
-        effect = new AudioClipWav("res/wav/powerup.wav");
         music = new AudioClipWav("res/wav/thegardens.wav");
-
         music.Start();
     }
 
     public static void OnUpdate(float delta)
     {
-        // audio test
-        if (Input.GetKeyDown(Key.I)) effect.Start();
-        if (Input.GetKeyDown(Key.O)) effect.PauseOrContinue();
-        if (Input.GetKeyDown(Key.P)) effect.Stop();
+        // pause music with space
+        if (Input.GetKeyDown(Key.Space)) music.PauseOrContinue();
     }
 
     public static void OnRender(float delta)
     {
         // draw background color
-        Drawing.SetColor(Color.CornflowerBlue);
+        Drawing.SetColor(Color.LightGray);
         Drawing.ClearWindow();
-
-        // draw red circle in the centre
-        Drawing.SetColor(Color.Red);
-        Drawing.DrawCircle(Vector2.Zero, 64);
-
-        // draw a white circle at the mouse position
-        Drawing.SetColor(Color.White);
-        Drawing.DrawCircleFilled(Input.GetMousePosition(), 16);
 
         // draw sprite
         Drawing.DrawSprite(sprite, Vector2.Zero, 8);
+
+        // draw a white circle at the mouse position
+        Drawing.SetColor(Color.Black);
+        Drawing.DrawCircleFilled(Input.GetMousePosition(), 8);
     }
 }
